7087aa79dfe7fdb953220acdc39e652ca1aedf74
ahmed bendary <31450453+KLQUInfinity@users.noreply.github.com>
2019-08-09 14:15:33 +0200

    bendary

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDamage : MonoBehaviour
+{
+
+    public float damage;
+    public float damageRate;
+    public float pushBackForce;
+
+    private float nextDamage;
+
+    // Use this for initialization
+    void Start()
+    {
+        nextDamage = 0f;
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag.Equals("Player") && nextDamage < Time.time)
+        {
+            PlayerHealth playerHealth = other.gameObject.GetComponent<PlayerHealth>();
+            playerHealth.addDamage(damage);
+            nextDamage = Time.time + damageRate;
+
+            pushBack(other.transform);
+        }
+    }
+
+    void pushBack(Transform pushedObject)
+    {
+        Vector2 pushDirection = new Vector2(0, 1f);
+        pushDirection *= pushBackForce;
+        Rigidbody2D pushRB = pushedObject.gameObject.GetComponent<Rigidbody2D>();
+        pushRB.velocity = Vector2.zero;
+        pushRB.AddForce(pushDirection, ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public float fullHealth;
+    public GameObject deathFX;
+
+    private float currentHealth;
+
+    private PlayerController controlMovement;
+
+    void Start()
+    {
+        currentHealth = fullHealth;
+
+        controlMovement = GetComponent<PlayerController>();
+    }
+
+    public void addDamage(float damage)
+    {
+        if (damage <= 0)
+        {
+            return;
+        }
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            makeDead();
+        }
+    }
+
+    void makeDead()
+    {
+        Instantiate(deathFX, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
